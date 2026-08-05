26db9d06eb24a862df349f24bc5d05f9a5902a59
enliple-wonyongwan <ywwon@enliple.com>
2026-08-05 19:05:54 +0900

    뉴스상세 페이지 사이드바 영역 고정 조건 스크립트 작성 및 추가 뉴스리스트 퍼블리싱

---
+++ assets/js/ui.js
@@ -0,0 +1,29 @@
+const $target = document.querySelector('[data-fix]');
+const $targetChild = document.querySelector('[data-fix-child]');
+function checkPosition() {
+  const targetRect = $target.getBoundingClientRect();
+  const targetChildRect = $targetChild.getBoundingClientRect();
+
+  const isTargetBottom = targetRect.bottom <= window.innerHeight && targetRect.bottom <= window.innerHeight - 30;
+  const isTargetChildTop = window.scrollY + window.innerHeight - 137 - 30 <= targetChildRect.height;
+  const isTargetChildBottom = targetChildRect.bottom <= window.innerHeight && targetChildRect.bottom <= window.innerHeight - 30;
+
+  if (isTargetChildBottom) {
+    $target.classList.add('is-fix');
+
+    if (isTargetBottom) {
+      $target.classList.remove('is-fix');
+      $target.classList.add('is-absolute');
+    }
+  } else {
+    $target.classList.remove('is-absolute');
+    $target.classList.add('is-fix');
+  }
+
+  if (isTargetChildTop) {
+    $target.classList.remove('is-absolute');
+    $target.classList.remove('is-fix');
+  }
+}
+
+window.addEventListener('scroll', checkPosition);
